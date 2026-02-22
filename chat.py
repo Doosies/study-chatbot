3a479b8868d5d3339749162d27a3b0d6b72c1f1c
Song_Minhyung <song961003@gmail.com>
2026-02-22 17:18:32 +0900

    feat: few shot

--- chat.py
+++ chat.py
@@ -9,19 +9,21 @@ st.title("챗봇!!")
 st.caption("소득세에 관련된 모든것을 답변해드립니다.")
 
 if 'message_list' not in st.session_state:
-  st.session_state.message_list = []
-  
+    st.session_state.message_list = []
+
 for message in st.session_state.message_list:
     with st.chat_message(message["role"]):
-      st.write(message["content"])
-      
+        st.write(message["content"])
+
 if user_question := st.chat_input(placeholder="입력해주세요"):
-  with st.chat_message("user"):
-    st.write(user_question)
-    st.session_state.message_list.append({"role": "user", "content": user_question})
-  
-  with st.spinner("답변 생성중..."):
-    ai_response = get_ai_response(user_question)
-    with st.chat_message("ai"):
-      ai_message = st.write_stream(ai_response)
-      st.session_state.message_list.append({"role": "ai", "content": ai_message})
+    with st.chat_message("user"):
+        st.write(user_question)
+        st.session_state.message_list.append(
+            {"role": "user", "content": user_question})
+
+    with st.spinner("답변 생성중..."):
+        ai_response = get_ai_response(user_question)
+        with st.chat_message("ai"):
+            ai_message = st.write_stream(ai_response)
+            st.session_state.message_list.append(
+                {"role": "ai", "content": ai_message})
--- llm.py
+++ llm.py
@@ -1,118 +1,139 @@
-
-from langchain_classic import hub
+from sre_parse import expand_template
 from langchain_classic.chains.history_aware_retriever import create_history_aware_retriever
 from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMessagePromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
-
 from langchain_core.runnables.history import RunnableWithMessageHistory
+
+from config import answer_examples
 
 store = {}
 
+
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
-  if session_id not in store:
-    store[session_id] = ChatMessageHistory()
-  return store[session_id]
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
 
 def get_llm(model='gpt-4o'):
-  llm = ChatOpenAI(model=model)
-  return llm
+    llm = ChatOpenAI(model=model)
+    return llm
 
 
 def get_retriever():
-  embedding = OpenAIEmbeddings(model="text-embedding-3-large")
-  index_name = "inflearn1"
-  data_base = PineconeVectorStore.from_existing_index(index_name,embedding)
-  retriever = data_base.as_retriever(search_kwargs={'k': 5})
-  
-  return retriever
+    embedding = OpenAIEmbeddings(model="text-embedding-3-large")
+    index_name = "inflearn1"
+    data_base = PineconeVectorStore.from_existing_index(index_name, embedding)
+    retriever = data_base.as_retriever(search_kwargs={'k': 5})
+
+    return retriever
+
+
+def get_history_retriever():
+    llm = get_llm()
+    retriever = get_retriever()
+
+    contextualize_q_system_prompt = (
+        "채팅 기록과 사용자의 최근 질문을 고려하여"
+        "채팅 기록의 맥락을 참조할 수 있는"
+        "채팅 기록 없이도 이해할 수 있는 독립적인 질문을 만드세요."
+        "질문에 직접 답변하지 말고,"
+        "필요한 경우 질문을 재구성하고, 그렇지 않으면 원래 질문 그대로 반환하세요."
+    )
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    return history_aware_retriever
+
 
 def get_dictionary_chain():
-  dictionary = ["사람을 나타내는 표현 -> 거주자"]
-  prompt = ChatPromptTemplate.from_template(f"""
-  사용자의 질문을 사전을 참고하여 수정하세요.
+    dictionary = ["사람을 나타내는 표현 -> 거주자"]
+    prompt = ChatPromptTemplate.from_template(f"""
+        사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
+        만약 변경할 필요가 없다고 판단된다면, 사용자의 질문을 변경하지 않아도 됩니다.
+        그런 경우에는 질문만 리턴해주세요
+        사전: {dictionary}
+        
+        질문: {{question}}
+    """)
+    llm = get_llm()
+    dictionary_chain = prompt | llm | StrOutputParser()
 
-  - 반드시 최종 질문 문장만 출력하세요.
-  - 설명, 이유, 따옴표, 접두어를 절대 포함하지 마세요.
-  - 변경이 필요 없으면 원문 질문만 그대로 출력하세요.
-    사전: {dictionary}
-    
-    질문: {{question}}
-  """)
-  llm = get_llm()
-  dictionary_chain = prompt | llm | StrOutputParser()
-  
-  return dictionary_chain
+    return dictionary_chain
+
 
 def get_rag_chain():
-  llm = get_llm()
-  retriever= get_retriever()
-  
-  contextualize_q_system_prompt = (
-    "채팅 기록과 사용자의 최근 질문을 고려하여"
-    "채팅 기록의 맥락을 참조할 수 있는"
-    "채팅 기록 없이도 이해할 수 있는 독립적인 질문을 만드세요."
-    "질문에 직접 답변하지 말고,"
-    "필요한 경우 질문을 재구성하고, 그렇지 않으면 원래 질문 그대로 반환하세요."
-  )
-  contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-      ("system", contextualize_q_system_prompt),
-      MessagesPlaceholder("chat_history"),
-      ("human", "{input}")
-    ]
-  )
-  history_aware_retriever = create_history_aware_retriever(
-    llm, retriever, contextualize_q_prompt
-  )
-  
-  system_prompt = (
-    "당신은 질문 답변 업무를 담당하는 보조자입니다."
-    "다음과 같은 맥락 정보를 활용하여 질문에 답하십시오."
-    "답을 모르는 경우, 모른다고 말하십시오."
-    "최대 세 문장으로 간결하게 답변하십시오." 
-    "\n\n"
-    "{context}"
-)
 
-  qa_prompt = ChatPromptTemplate.from_messages(
-      [
-          ("system", system_prompt),
-          MessagesPlaceholder("chat_history"),
-          ("human", "{input}"),
-      ]
-  )
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{answer}")
+        ]
+    )
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=answer_examples
+    )
+    system_prompt = (
+        "당신은 소득세법 전문가입니다. 사용자의 소득세법에 관한 질문에 답변해주세요"
+        "아래에 제공된 문서를 활용해서 답변해주시고"
+        "답변을 알 수 없다면 모른다고 답변해주세요"
+        "답변을 제공할 때는 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고"
+        "2-3 문장정도의 짧은 내용의 답변을 원합니다"
+        "\n\n"
+        "{context}"
+    )
 
-  question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            few_shot_prompt,
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
 
-  rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    llm = get_llm()
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
+    history_aware_retriever = get_history_retriever()
+    rag_chain = create_retrieval_chain(
+        history_aware_retriever, question_answer_chain)
 
-  conversational_rag_chain = RunnableWithMessageHistory(
-      rag_chain,
-      get_session_history,
-      input_messages_key="input",
-      history_messages_key="chat_history",
-      output_messages_key="answer",
-  )
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
 
-  return conversational_rag_chain
+    return conversational_rag_chain
+
 
 def get_ai_response(user_message: str):
-  dictionary_chain = get_dictionary_chain()
-  rag_chain = get_rag_chain()
-  tax_chain = {"input": dictionary_chain} | rag_chain
-  ai_response = tax_chain.stream(
-    {"question": user_message},
-    config={"configurable": {"session_id": "abc123"}}
-  )
-  
-  for chunk in ai_response:
-    if "answer" in chunk:
-      yield chunk['answer']
-  
+    dictionary_chain = get_dictionary_chain()
+    rag_chain = get_rag_chain()
+    tax_chain = {"input": dictionary_chain} | rag_chain
+    ai_response = tax_chain.stream(
+        {"question": user_message},
+        config={"configurable": {"session_id": "abc123"}}
+    )
+
+    for chunk in ai_response:
+        if "answer" in chunk:
+            yield chunk['answer']
